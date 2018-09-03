75ce84151ca5a264594f18446f17b1ecd74d4c21
X901 <im.x901@gmail.com>
2018-09-03 20:34:56 +0300

    Regular Expression (Number Only)
    This is a simple prject on how to use Regular Expression in Swift 4.x
    to display only Numbers

--- OnlyNumbers/ViewController.swift
+++ OnlyNumbers/ViewController.swift
@@ -9,17 +9,73 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    let regularExpression = RegularExpression()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+    }
+    
+    @IBAction func checkTapped(_ sender: UIButton) {
+        
+      let string =  checkText(string: textField.text!)
+        
+        print(string)
+    }
+    
+
+    func checkText(string: String) -> Bool  {
+        
+
+        let matched = regularExpression.matches(for: "^[0-9]+$", in: string)
+        
+        
+        if matched != [] {
+      
+            return true
+         
+        }else {
+            
+            let alert = UIAlertController(title: "Warning", message: "It Should Be Numbers Only!", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+            }))
+            self.present(alert, animated: true, completion: nil)
+
+           return false
+        }
     }
 
 
 }
+
+class RegularExpression  {
+    
+    func matches(for regex: String, in text: String) -> [String] {
+        
+        do {
+            let regex = try NSRegularExpression(pattern: regex)
+            let results = regex.matches(in: text,
+                                        
+           range: NSRange(text.startIndex..., in: text))
+            
+            var newString = ""
+            return results.map {
+                if let text2 = Range($0.range, in: text) {
+                    newString = String(text[text2])
+                }
+                return newString
+                
+            }
+            
+        } catch let error {
+            print("invalid regex: \(error.localizedDescription)")
+            return []
+        }
+}
+}
+
+
 
