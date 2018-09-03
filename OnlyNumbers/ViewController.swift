c42e9f7885727b1ebd59a7184c1c8732c8e63392
X901 <im.x901@gmail.com>
2018-09-03 19:09:42 +0300

    Initial Commit

---
+++ OnlyNumbers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OnlyNumbers
+//
+//  Created by X901 on 03/09/2018.
+//  Copyright © 2018 X901. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
